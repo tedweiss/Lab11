348c0146b106f7c42b54c136a07d9a2efbeb9fda
Minkah McKay <abletoedit@gmail.com>
2015-09-10 11:01:59 -0400

    almost there, all three together

--- seattingapp.js
+++ seattingapp.js
@@ -1,24 +1,30 @@
-
-var $formContainer = $("#reservationForm");
-
-var $seatContainer = $("#seats");
-
-var theatre = {
-	name: 'Grand Circus Theatre',
-	numberOfSeats: 24
-}
-
-
 $(document).ready(function(){
  
- 
+	 
+	var $currentSeat = {};
 
-$('.seat').click(function(){
-	
-	$('.reservationForm').show();
-	$(this).toggleClass('seatSelected');
-});
+	$('.seat').click(function(){
+		
+		$('.reservationForm').show();
+		$(this).toggleClass('seatSelected');
+		$currentSeat = $(this);
+	});
 
+	$('.seatSelected').mouseover(function(){
+		console.log("yay")
+	});
 
+	$('button').click(function() {
+		console.log("hi mom");
+		var seatName = $("#exampleInputName2").val();
+		alert ("This seat is reserved for " + seatName);
+		$currentSeat.patronName=seatName;
+		$currentSeat.hover(function(){
+			$currentSeat.children().after($currentSeat.patronName);
+		}, function(){
+			$currentSeat.children().remove($currentSeat.patronName)
+
+		});
+	});
 
 }); 
